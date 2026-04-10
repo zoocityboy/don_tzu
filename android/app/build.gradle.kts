949a1a585ab721f6ee7bed9b96dfa0657f2e1470
zoocityboy <zoocityboy@gmail.com>
2026-04-10 18:20:53 +0200

    fix(android, ios): update application label to "The Art of Deal War"
    * Changed AndroidManifest.xml to reflect the correct app label.
    * Updated Info.plist for iOS to match the new app label.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,6 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.dealwar.art_of_deal_war"
-        name = "The Art of Deal War"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
